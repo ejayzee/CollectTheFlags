02e9d1b0692c877db6aec842aed3413fd40047d7
EMMANUEL J ZAFRA <ejayzee@mac.com>
2016-11-23 03:14:08 -0800

    Map working and Compass button centers location

--- CollectTheFlags/ViewController.swift
+++ CollectTheFlags/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go!")
+            mapView.showsUserLocation = true
+            
+            manager.startUpdatingLocation()
+            
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if updateCount < 1 {
+        let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 30, 30)
+        mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
     }
-
-
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        
+        if let coord = manager.location?.coordinate {
+        let region = MKCoordinateRegionMakeWithDistance(coord, 30, 30)
+        mapView.setRegion(region, animated: true)
+        }
+    }
+    
 }
 
